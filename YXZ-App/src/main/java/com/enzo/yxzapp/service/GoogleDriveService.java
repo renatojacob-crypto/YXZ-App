5d65a2ffd466f5180fccf7b2b72dc55ab2e7d35d
Enzo Sampaio <enzosenzos9@gmail.com>
2026-03-11 13:35:19 -0300

    Update GoogleDriveService.java

--- YXZ-App/src/main/java/com/enzo/yxzapp/service/GoogleDriveService.java
+++ YXZ-App/src/main/java/com/enzo/yxzapp/service/GoogleDriveService.java
@@ -1,7 +1,39 @@
 package com.enzo.yxzapp.service;
 
+import com.google.api.client.http.InputStreamContent;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.Collections;
 
 @Service
 public class GoogleDriveService {
+    @Value("${google.drive.folder.id}")
+    private String pastaMaeId;
+
+    private Drive getDriveService() throws IOException {
+        GoogleCredentials credentials = GoogleCredentials.fromStream(
+                new ClassPathResource("credentials.json").getInputStream()
+        ).createScoped(Collections.singletonList(DriveScopes.DRIVE_FILE));
+
+        return new Drive.Builder(
+                new NetHttpTransport(),
+                GsonFactory.getDefaultInstance(),
+                new HttpCredentialsAdapter(credentials)
+        ).setApplicationName("YXZ-App").build();
+    }
+
+
+
+
+
 }
